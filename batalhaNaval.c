4e3f4f31553be1486bd56979e55f727da81b3ec3
Rivaldo <160893472+RivaldoSantos1250@users.noreply.github.com>
2025-11-28 23:52:28 +0000

    Nivel-Mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -20,28 +20,63 @@ int main() {
     printf("\n");
 
     // Colocar navio na horizontal
-    for (int j = 4; j <= 6; j++) {  
-        tabuleiro[2][j] = 3;
-    }
+    /* for (int j = 4; j <= 6; j++) {  
+            tabuleiro[2][j] = 3;
+        }
 
-    // Colocar navio na vertical
-    for (int i = 4; i <= 6; i++) {  
-        tabuleiro[i][7] = 3;
-    }
+        // Colocar navio na vertical
+        for (int i = 4; i <= 6; i++) {  
+            tabuleiro[i][7] = 3;
+        }
 
-    // Colocar navio diagonal principal
+        // Colocar navio diagonal principal
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i == j && i > 0 && i <= 3) {
+                    tabuleiro[i][j] = 3;
+                }
+            }
+        }
+
+        // Colocar navio diagonal secundária
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i + j == 9 && i >= 4 && i <= 6) {
+                    tabuleiro[i][j] = 3;
+                }
+            }
+        } */
+
+    // Colocar habilidade cone
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            if (i == j && i > 0 && i <= 3) {
+            if (i == 0 && j == 2) {
+                tabuleiro[i][j] = 3;
+            } else if (i == 1 && j >= 1 && j <= 3) {
+                tabuleiro[i][j] = 3;
+            } else if (i == 2 && j >= 0 && j <= 4) {
                 tabuleiro[i][j] = 3;
             }
         }
     }
 
-    // Colocar navio diagonal secundária
+    // Colocar habilidade cruz 
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            if (i + j == 9 && i >= 4 && i <= 6) {
+            if (i == 6 && j >= 1 && j <= 5) {
+                tabuleiro[i][j] = 3;
+            } else if (i >= 5 && i <= 7 && j == 3) {
+                tabuleiro[i][j] = 3;
+            }
+        }
+    }
+
+    // Colocar habilidade octaedro 
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (i == 2 && j >= 6 && j <= 8) {
+                tabuleiro[i][j] = 3;
+            } else if (i >= 1 && i <= 3 && j == 7) {
                 tabuleiro[i][j] = 3;
             }
         }
